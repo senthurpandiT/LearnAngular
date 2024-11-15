e29dc0970c54ec898307549067fcc3446dd5f3b3
senthurpandiT <senthurpandi@securenext.in>
2024-11-15 12:20:19 +0530

    At scratch

--- src/app/core/Injections/dog.service.ts
+++ src/app/core/Injections/dog.service.ts
@@ -1,10 +1,93 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+export type Item = {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly age: number;
+  readonly favColor: string;
+  readonly petName: string;
+};
+
+export type ItemsTableHeader = {
+  path: keyof Item;
+  label: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DogService {
+  readonly $headers = signal<ItemsTableHeader[]>([]);
+  readonly $sortByHeader = signal<keyof Item | undefined>(undefined);
+  readonly $sortingOrder = signal<'asc' | 'desc'>('asc');
+  readonly $itemsPerPage = signal<number>(5);
+  readonly $page = signal<number>(0);
 
+  private readonly $inputItems = signal<Item[]>([]);
 
+  readonly $sortedItems = computed(() => {
+    const sortPath = this.$sortByHeader();
+    if (!sortPath) {
+      return this.$inputItems();
+    }
+    const items = this.$inputItems().slice();
+
+    items.sort((a, b) => {
+      const aVal = a[sortPath];
+      const bVal = b[sortPath];
+      if (typeof aVal === 'number' && typeof bVal === 'number') {
+        return aVal < bVal ? -1 : (aVal > bVal ? 1 : 0);
+      } else {
+        return a[sortPath].toString().localeCompare(b[sortPath].toString());
+      }
+    });
+
+    if (this.$sortingOrder() === 'asc') {
+      return items;
+    } else {
+      return items.reverse();
+    }
+  });
+
+  readonly $items = computed(() => {
+    const page = this.$page();
+    const items = this.$sortedItems();
+    const perPage = this.$itemsPerPage();
+    if (page === 0 && items.length <= perPage) {
+      return items;
+    }
+    return items.slice(page * perPage, page * perPage + perPage);
+  });
+
+  setItems(items: Item[]) {
+    this.$inputItems.set(items.slice());
+  }
+
+  changeSorting(path: keyof Item) {
+    if (path === this.$sortByHeader()) {
+      if (this.$sortingOrder() === 'asc') {
+        this.$sortingOrder.set('desc');
+      } else {
+        this.$sortByHeader.set(undefined);
+      }
+    } else {
+      this.$sortByHeader.set(path);
+      this.$sortingOrder.set('asc');
+    }
+  }
+
+  prevPage() {
+    const page = this.$page();
+    if (page > 0) {
+      this.$page.set(page - 1);
+    }
+  }
+
+  nextPage() {
+    const page = this.$page();
+    const items = this.$sortedItems();
+    if (items.length > ((page + 1) * this.$itemsPerPage())) {
+      this.$page.set(page + 1);
+    }
+  }
 }
--- src/app/core/child/child.component.ts
+++ src/app/core/child/child.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
-import { Employee } from '../parent/parent.component';
-import { DogService } from '../Injections/dog.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -11,44 +9,10 @@ import { DogService } from '../Injections/dog.service';
 
 })
 export class ChildComponent {
-
-
-  // newEmployee: Employee | null = new Employee('senthur', 456);
-  @Input() childData: any;
-
-  constructor() { }
+    constructor() { }
 
   ngOnInit() {
 
-
-    if (this.childData) {
-      console.log(this.childData, 'ChildComponent');
-    } else {
-      console.log(this.childData, 'ChildComponent');
-    }
   }
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   console.log('ngOnChanges called');
-  // }
-  // ngOnDestroy(): void {
-  //   console.log('ngOnDestroy called');
-  // }
-  update() {
-    const A = {
-      id: this.childData.legth++,
-      name: "Buddy",
-      breed: "Golden Retriever",
-      age: 3,
-      description: "Friendly and energetic.",
-      long_description: "Buddy is a friendly Golden Retriever who loves to play outdoors and enjoys long walks. He is very loyal and loves to be around people.",
-      location: "New York, NY"
-    }
-    this.childData.push(A);
-    console.log(this.childData);
 
-    setTimeout(() => {
-
-      // this.childData.shift(A);
-    }, 2000);
-  }
 }
